efa5a6d0c18b85dce25f6e89fe13ba69ea5bebab
Rajat <kaushikrajat777.com>
2023-02-01 17:28:47 +0530

    ...

--- src/Components/EmptyPage/index.js
+++ src/Components/EmptyPage/index.js
@@ -1,9 +1,10 @@
 import React, { Component } from 'react'
+import './index.css'
 
 export default class EmptyPage extends Component {
   render() {
     return (
-      <div>There is no history to show</div>
+      <div className='empty-text' >There is no history to show</div>
     )
   }
 }
--- src/Components/HistoryPage/index.js
+++ src/Components/HistoryPage/index.js
@@ -2,40 +2,44 @@ import React, { Component } from 'react'
 import EmptyPage from '../EmptyPage';
 import HistoryList from '../HistoryItem';
 import SearchBar from '../SearchBar';
+import './index.css'
 
 export default class HistoryPage extends Component {
 
-    state={listToShow:this.props.historyList,
-        historyList:this.props.historyList
+    state = {
+        listToShow: this.props.historyList,
+        historyList: this.props.historyList
     };
 
-    deleteItem=(id)=>{
-        this.setState({listToShow:this.state.listToShow.filter((obj)=>obj.id!==id)});
+    deleteItem = (id) => {
+        this.setState({ listToShow: this.state.listToShow.filter((obj) => obj.id !== id) });
     }
 
-    handleInput=(value)=>{
-        const newList=this.state.historyList;
-        this.setState({listToShow:newList.filter(obj=>{
-            return obj.title.includes(value);
-        })});
+    handleInput = (value) => {
+        const newList = this.state.historyList;
+        this.setState({
+            listToShow: newList.filter(obj => {
+                return obj.title.includes(value);
+            })
+        });
     }
 
     render() {
         return (
             <div className="main-container">
-                 
+
                 <SearchBar handleInput={this.handleInput} />
 
                 <div className="history-list-container">
-                    
-                {this.state.listToShow.length>0 && this.state.listToShow.map((item) => (
-                    
-                    <HistoryList onclick={()=>this.deleteItem(item.id)} item={item}/>
-                ))
+
+                    {this.state.listToShow.length > 0 && this.state.listToShow.map((item) => (
+
+                        <HistoryList onclick={() => this.deleteItem(item.id)} item={item} />
+                    ))
+                    }
+                </div>
+                {this.state.listToShow.length === 0 && <EmptyPage />
                 }
-                {this.state.listToShow.length===0 && <EmptyPage/>
-                }
-            </div>
             </div>
         )
     }
